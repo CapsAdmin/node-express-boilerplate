9196c1eb14bdf492b1a4c7a73a9fc54c1ebe3aef
CapsAdmin <eliashogstvedt@gmail.com>
2021-05-03 12:53:57 +0200

    first step towards using typescript

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,8 @@ module.exports = {
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+  ],
 };
