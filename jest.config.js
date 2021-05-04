529b19c1d552a7b1ded51c47c98e0453c56e9c3f
CapsAdmin <eliashogstvedt@gmail.com>
2021-05-04 11:22:01 +0200

    add typescript support to jest ignore ts errors for now

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,13 @@ module.exports = {
     NODE_ENV: 'test',
   },
   restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
+  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-  ],
+  globals: {
+    "ts-jest": {
+      "diagnostics": false
+    }
+  },
+  transform: { '\\.ts$': ['ts-jest'] },
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
 };
